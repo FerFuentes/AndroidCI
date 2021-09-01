38379b2bd1c081f7260032321f7702924df6a696
Fer Fuentes <lzfer1@me.com>
2021-09-01 12:21:01 -0700

    Added App center SDK

--- app/src/main/java/com/e/androidci/MainActivity.kt
+++ app/src/main/java/com/e/androidci/MainActivity.kt
@@ -2,10 +2,18 @@ package com.e.androidci
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(
+            application, "766129a0-b033-4396-a782-f0ce47d081d3",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
